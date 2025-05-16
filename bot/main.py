9d6ffe592708271de6f05f017341c095901b26f8
slpc6 <jmoralesgomez0106@gmail.com>
2025-05-16 16:25:43 -0500

    feat: exponer el puerto para despliegue

--- bot/main.py
+++ bot/main.py
@@ -3,9 +3,13 @@
 #External libraries
 import discord
 import os
+import uvicorn
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from config import config
 from path import path
-
 from discord.ext import commands
 
 
@@ -13,7 +17,14 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix=config.PREFIX, intents=intents)
-
+app = FastAPI(version="1.0.0", title="Discord bot", description="")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 async def load_cogs():
     """Cargar los cogs del bot.
@@ -33,5 +44,9 @@ async def on_ready():
 """
     print(f'Bot conectado como {bot.user}')
     await load_cogs()
+
+if __name__ == "__main__":
+    """Punto de inicio de la aplicacion, corre el servidor."""
+    uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=True)
 
 bot.run(config.TOKEN)
